6ee959850a9ba2b327c4f8914ee7b197866c8171
AlonsoFariasNavarro <alonso.farias@usm.cl>
2023-06-02 21:25:38 -0400

    view agregar

--- resources/views/estudiante/agregar.blade.php
+++ resources/views/estudiante/agregar.blade.php
@@ -1,8 +1,10 @@
 @extends('templates.master')
-@section('titulo','agregar')
 
+@section('hojas-estilo')
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+@endsection
 
-@section('body')
+@section('contenido-principal')
 <div class="container d-flex align-items-center justify-content-center">    
         <div class="row-12">
             <div class="col-12 px-6 py-5 ">
