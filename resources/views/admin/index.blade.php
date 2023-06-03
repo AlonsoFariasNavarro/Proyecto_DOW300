f78bb3bf1c4f66bb8ad7f9a315098036cc577cb0
AlonsoFariasNavarro <alonso.farias@usm.cl>
2023-06-02 21:56:58 -0400

    a

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,18 +1,11 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
-</head>
-<body>
-   <div class="col">
-        <div class="row">
-            <div>
-                <h3>holaaaaa</h3>
-            </div>
-        </div>
-   </div>
-</body>
-</html>
+@extends('templates.master')
+
+@section('hojas-estilo')
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+@endsection
+
+@section('contenido-principal')
+    <div class="container-fluid">
+        hola
+    </div>
+@endsection
